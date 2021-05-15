a6f631761728f3c2a63ffa5654838e61823b66c4
Igor Buduev <igrich@gmail.com>
2021-05-15 21:50:05 +0300

    addition link on post

--- hello.py
+++ hello.py
@@ -15,3 +15,17 @@ def get_db_connection():
     conn = sqlite3.connect('db/database.db')
     conn.row_factory = sqlite3.Row
     return conn
+
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',
+                        (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
+
+@app.route('/<int:post_id>')
+def post(post_id):
+    post = get_post(post_id)
+    return render_template('post.html', post=post)
