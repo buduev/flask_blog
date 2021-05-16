cc167105f6da86a41fef619b593354dbe5254d99
Igor Buduev <igrich@gmail.com>
2021-05-16 14:37:06 +0300

    upd docker files

--- app/hello.py
+++ app/hello.py
@@ -3,16 +3,21 @@ from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
 import logging
 import os
+import json
+import random
+import time
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    conn = get_db_connection()
-    posts = conn.execute('SELECT * FROM posts').fetchall()
-    conn.close()    
-    logging.debug("Текущая деректория:", os.getcwd())
-    return render_template('index.html', posts = posts)
+
+FAIL_RATE=float(os.environ.get('FAIL_RATE', '0.05'))
+SLOW_RATE=float(os.environ.get('SLOW_RATE', '0.00'))
+
+def do_staff():
+    time.sleep(random.gammavariate(alpha=1.5, beta=.1))
+
+def do_slow():
+    time.sleep(random.gammavariate(alpha=30, beta=0.3))
 
 def get_db_connection():
     conn = sqlite3.connect('app/db/database.db')
@@ -28,11 +33,33 @@ def get_post(post_id):
         abort(404)
     return post
 
+
+@app.route('/')
+def hello():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()    
+    logging.debug("Текущая деректория:", os.getcwd())
+    return render_template('index.html', posts = posts)
+
+
 @app.route('/<int:post_id>')
 def post(post_id):
     post = get_post(post_id)
     return render_template('post.html', post=post)
 
 
+@app.route('/probe')
+def probe():
+    if random.random() < FAIL_RATE:
+        abort(500)
+    if random.random() < SLOW_RATE:
+        do_slow()
+    else:
+        do_staff()
+    return "OK"
+
+
+
 if __name__ == "__main__":
-    app.run(debug=True, host='127.0.0.1')
+    app.run(host='0.0.0.0', port='5001', debug=True)
